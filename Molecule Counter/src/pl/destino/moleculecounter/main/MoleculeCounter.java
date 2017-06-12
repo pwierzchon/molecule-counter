74f9814998a6b97a07b70828008cf1f668028580
Destino <Destino@192.168.0.14>
2017-06-12 23:43:33 +0200

    Added parenthesis recognition and multiplying elements inside them

--- Molecule Counter/src/pl/destino/moleculecounter/main/MoleculeCounter.java
+++ Molecule Counter/src/pl/destino/moleculecounter/main/MoleculeCounter.java
@@ -21,32 +21,69 @@ public class MoleculeCounter {
 
     public Map<String, Integer> calculateMolecules(String input) {
 
+        matchStrings(input);
+        
+        return results;
+    }
+    
+    private void matchStrings(String input, int multip){
         //Create Matcher
         Pattern pattern = Pattern.compile("([A-Z]{1}[a-z]*[0-9]*)");
-        Matcher matcher = pattern.matcher(input);
-
-        Pattern finalPattern = Pattern.compile("([A-Z]{1}[a-z]*)([0-9]+)");
+        Pattern multPat = Pattern.compile("([A-Z]{1}[a-z]*[0-9]*)|([(]{1}([A-Z]{1}[a-z]*[0-9]*)+[)]{1}[0-9]+)");
+        Pattern multiplierPattern = Pattern.compile("([)]{1}([0-9]))");
+        Pattern innerPattern = Pattern.compile("([A-Z]+[0-9]*)+");
+        Matcher multMacher = multPat.matcher(input);
 
         //Find elements with numbers
-        while (matcher.find()) {
-            String semiRes = matcher.group();
-            Matcher finalMatcher = finalPattern.matcher(semiRes);
+        while (multMacher.find()) {
+            int multiplier = multip;
 
-            //Check if single element
-            if (semiRes.length() == 1) {
-                //add to results
-                insertElements(semiRes, 1);
+            String semiRes = multMacher.group();
+            String multGroup = multMacher.group(2);
+
+            //check if there's a 2 group
+            if (null != multGroup) {
+                Matcher multiplierMacher = multiplierPattern.matcher(multGroup);
+                Matcher innerMatcher = innerPattern.matcher(multGroup);
+                while (multiplierMacher.find()) {
+                    multiplier = Integer.parseInt(multiplierMacher.group(2));
+                    while (innerMatcher.find()) {
+                        matchStrings(innerMatcher.group(), multiplier);
+                    }
+
+                }
             } else {
-                while (finalMatcher.find()) {
-                    String element = finalMatcher.group(1);
-                    Integer count = Integer.parseInt(finalMatcher.group(2));
-                    insertElements(element, count);
-
+                //Check if single element
+                if (semiRes.length() == 1) {
+                    //add to results
+                    insertElements(semiRes, multiplier);
+                } else {
+                    finalMatch(semiRes);
                 }
             }
 
         }
-        return results;
+    }
+
+    private void matchStrings(String input) throws NumberFormatException {
+        matchStrings(input, 1);
+    }
+
+    private void finalMatch(String semiResult) throws NumberFormatException {
+        finalMatch(semiResult, 1);
+    }
+
+    private void finalMatch(String semiResult, int multiplier) {
+        System.out.println("semi res: " + semiResult);
+        Pattern finalPattern = Pattern.compile("([A-Z]{1}[a-z]*)([0-9]+)");
+        Matcher finalMatcher = finalPattern.matcher(semiResult);
+
+        while (finalMatcher.find()) {
+            String element = finalMatcher.group(1);
+            Integer count = Integer.parseInt(finalMatcher.group(2)) * multiplier;
+            insertElements(element, count);
+
+        }
     }
 
     private void insertElements(String element, Integer count) {
